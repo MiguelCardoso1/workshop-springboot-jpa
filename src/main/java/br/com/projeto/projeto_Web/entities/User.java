2085111efa75808ee02e54960298d576a231309e
Miguel Malton <miguel.malton@gmail.com>
2024-07-30 11:19:52 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/br/com/projeto/projeto_Web/entities/User.java
+++ src/main/java/br/com/projeto/projeto_Web/entities/User.java
@@ -3,6 +3,8 @@ package br.com.projeto.projeto_Web.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +18,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -67,6 +72,9 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -80,5 +88,4 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
-
 }
